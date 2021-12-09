b84547bd5bd463d7f38e9a00f951437b3c46bdc5
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-09 17:13:09 -0500

    complete ProductContext so app consumed data, create CartContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// import context
+import ProductContext from './contexts/ProductContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -10,6 +13,9 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+
+	// check to make sure we have access to ProductContext
+	// console.log(ProductContext);
 
 	const addItem = item => {
 		// add the given item to the cart
@@ -22,9 +28,11 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/">
+					<Products />
+				</Route>
+			</ProductContext.Provider>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// consuming and add useContext above from React
+import ProductContext from '../contexts/ProductContext';
 
 // Components
 import Product from './Product';
 
-const Products = props => {
+const Products = (props) => {
+	// make use of useContext, destructure and remove props
+	const { products } = useContext(ProductContext);
+	// console.log(values);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
 				/>
 			))}
 		</div>
